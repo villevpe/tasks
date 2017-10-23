196951177a69dc1d634a5bf67c0e8e4bb2b288b7
villevpe <villevpe@users.noreply.github.com>
2017-10-23 20:55:16 +0300

    Setup enzyme, first component tests

---
+++ src/initTests.js
@@ -0,0 +1,9 @@
+global.requestAnimationFrame = (cb) => {
+  setTimeout(cb, 0)
+}
+
+const Enzyme = require('enzyme');
+const EnzymeAdapter = require('enzyme-adapter-react-16');
+
+// Setup enzyme's react adapter
+Enzyme.configure({ adapter: new EnzymeAdapter() });
