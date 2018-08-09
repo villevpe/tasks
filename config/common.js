580c151376544648d683400d32d73fbfd3f35ea1
villevpe <villevpe@users.noreply.github.com>
2018-08-09 22:10:23 +0300

    Include outside environment variables to build

--- config/common.js
+++ config/common.js
@@ -49,6 +49,7 @@ module.exports = {
         new webpack.DefinePlugin({
             'process.env': {
                 NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+                ...process.env,
                 ...environmentVariables
             }
         })
